f6d55c982d7defbc849fdbde1287af2336cf7b49
Мария <marymoldn@gmail.com>
2019-10-01 20:18:08 +0300

    Добавьте файлы проекта.

---
+++ AlcoMarket/Tables/Sales_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sales]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductID] INT NOT NULL, 
+    [CustomerID] INT NOT NULL, 
+    [Data] DATE NOT NULL, 
+    [Cost] FLOAT NOT NULL
+	FOREIGN KEY 
+)
